4b4e26acf9d77268c77dfd98085e6ed034655603
Peter Hoang <peterhg7@gmail.com>
2019-11-26 15:45:29 -0800

    Finished GroceryItemOrder Class

---
+++ Assign4/GroceryItemOrder.java
@@ -0,0 +1,27 @@
+public class GroceryItemOrder {
+
+    String name;
+    int quantity;
+    double pricePerUnit;
+
+    public GroceryItemOrder(String name, int quantity, double pricePerUnit)
+    {
+        this.name = name;
+        this.quantity = quantity;
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public double getCost()
+    {
+        return pricePerUnit * quantity;
+    }
+
+    public void setQuantity(int quantity)
+    {
+        this.quantity = quantity;
+    }
+
+
+
+
+}
